09830fb6de728b0594ea450d70842a22f207cbb1
Mayhm <nisekoimylove@gmail.com>
2023-11-13 01:26:14 -0300

    refactor (clients): ajuste nos modelos

--- Vendas/clients/models.py
+++ Vendas/clients/models.py
@@ -7,11 +7,11 @@ class Cliente(models.Model):
         default=uuid.uuid4, editable=False, unique=True, primary_key=True
     )
     nome = models.CharField(max_length=100)
-    rg = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=100)
+    rg = models.CharField(max_length=30)
+    cpf = models.CharField(max_length=20)
     email = models.CharField(max_length=100)
-    telefone = models.CharField(max_length=100)
-    celular = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=15)
+    celular = models.CharField(max_length=15)
     cep = models.CharField(max_length=100)
     endereco = models.CharField(max_length=200)
     numero = models.IntegerField()
@@ -19,3 +19,4 @@ class Cliente(models.Model):
     bairro = models.CharField(max_length=50)
     cidade = models.CharField(max_length=50)  
     uf = models.CharField(max_length=50)
+    
