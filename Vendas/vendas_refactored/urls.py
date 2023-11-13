b6f7ca8e8e803fd78668b3f8a7a0788d3b701103
Mayhm <nisekoimylove@gmail.com>
2023-11-13 01:25:16 -0300

    refactor: remoção de itens não utilizados

--- Vendas/vendas_refactored/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, salvar, clients, salvarCliente
-
-urlpatterns = [
-    path("", home),
-    path("salvar/", salvar, name="salvar"),
-    path("clients/", clients, name="clients"),
-    path("salvarCliente/", salvarCliente, name="salvarCliente"),
-]
