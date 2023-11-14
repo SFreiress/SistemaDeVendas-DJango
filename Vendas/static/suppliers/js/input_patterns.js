4ce94b508fd9deedc5e4d6a2a9b5b285fe3831bb
Mayhm <nisekoimylove@gmail.com>
2023-11-13 21:48:47 -0300

    refactor: ajustado para o padrão

--- Vendas/static/suppliers/js/input_patterns.js
+++ Vendas/static/suppliers/js/input_patterns.js
@@ -1,15 +1,6 @@
 $(document).ready(function () {
   $("#telefoneInput").inputmask("(99) 9999-9999");
-});
-
-$(document).ready(function () {
   $("#celularInput").inputmask("(99) 9 9999-9999");
-});
-
-$(document).ready(function () {
   $("#cepInput").inputmask("99 999-999");
-});
-
-$(document).ready(function () {
   $("#cnpjInput").inputmask("99.999.999/9999-99");
 });
