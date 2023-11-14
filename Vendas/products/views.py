a172af9f71d4ccfe38c1c9d2626b83f1db396aa3
Mayhm <nisekoimylove@gmail.com>
2023-11-13 21:41:50 -0300

    feat: list, create e edit de produtos + style

--- Vendas/products/views.py
+++ Vendas/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from .models import Produto
 
@@ -8,7 +8,7 @@ def home(request):
     produtos = Produto.objects.all()
     return render(request, "products.html", {"produtos": produtos})
 
-def salvar(request):
+def create(request):
     if request.method == 'POST':
         Produto.objects.create(
             descricao=request.POST.get("descricao"),
@@ -16,6 +16,17 @@ def salvar(request):
             qtd_estoque=request.POST.get("qtd_estoque")
         )
         return redirect('products:home')
+
+    return render(request, "create.html")
+
+def edit(request, id):
+    produto = get_object_or_404(Produto, pk=id)
     
-    produtos = Produto.objects.all()
-    return render(request, "products.html", {"produtos": produtos})
+    if request.method == 'POST':
+        produto.descricao = request.POST.get("descricao")
+        produto.preco = request.POST.get("preco")
+        produto.qtd_estoque = request.POST.get("qtd_estoque")
+        produto.save()
+        return redirect('products:home')
+    
+    return render(request, "edit.html", {"produto": produto})
