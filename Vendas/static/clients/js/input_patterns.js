f1d419a89d9af807f06e32115555f64b52c3cced
Roger-Pedroso <94937235+Roger-Pedroso@users.noreply.github.com>
2023-11-15 20:17:26 -0300

    Feat: busca cep

--- Vendas/static/clients/js/input_patterns.js
+++ Vendas/static/clients/js/input_patterns.js
@@ -19,4 +19,28 @@ $(document).ready(function () {
         .css("color", "red");
     }
   });
+
+  $("#cepInput").on("keypress", async function (e) {
+    console.log("CAIU" + e.which);
+    if (e.which === 13) {
+      var cep = $(this)
+        .val()
+        .replace(/[^0-9]/g, "");
+
+      try {
+        const request = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await request.json();
+
+        $("#enderecoInput").val(data.logradouro);
+        $("#bairroInput").val(data.bairro);
+        $("#cidadeInput").val(data.localidade);
+        $("#ufInput").val(data.uf);
+        $("#estadoInput").val(data.uf);
+
+        console.log(data);
+      } catch (error) {
+        console.error("Erro na requisição:", error);
+      }
+    }
+  });
 });
--- Vendas/static/suppliers/js/input_patterns.js
+++ Vendas/static/suppliers/js/input_patterns.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
   $("#cepInput").inputmask("99 999-999");
   $("#cnpjInput").inputmask("99.999.999/9999-99");
 
-  $('#numeroInput').on('input', function() {
+  $("#numeroInput").on("input", function () {
     $(this).val(function (index, value) {
-        return value.replace(/\D/g, '');
+      return value.replace(/\D/g, "");
     });
   });
 
@@ -22,6 +22,30 @@ $(document).ready(function () {
       $("#emailMessage")
         .text("Endereço de e-mail inválido")
         .css("color", "red");
+    }
+  });
+
+  $("#cepInput").on("keypress", async function (e) {
+    console.log("CAIU" + e.which);
+    if (e.which === 13) {
+      var cep = $(this)
+        .val()
+        .replace(/[^0-9]/g, "");
+
+      try {
+        const request = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await request.json();
+
+        $("#enderecoInput").val(data.logradouro);
+        $("#bairroInput").val(data.bairro);
+        $("#cidadeInput").val(data.localidade);
+        $("#ufInput").val(data.uf);
+        $("#estadoInput").val(data.uf);
+
+        console.log(data);
+      } catch (error) {
+        console.error("Erro na requisição:", error);
+      }
     }
   });
 });
