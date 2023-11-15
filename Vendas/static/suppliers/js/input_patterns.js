8bc22bc9fd3011c2ab7098ccf875dec4a2930eb0
Mayhm <nisekoimylove@gmail.com>
2023-11-14 21:20:00 -0300

    refactor (suppliers): estrutura, styles

--- Vendas/static/suppliers/js/input_patterns.js
+++ Vendas/static/suppliers/js/input_patterns.js
@@ -3,4 +3,25 @@ $(document).ready(function () {
   $("#celularInput").inputmask("(99) 9 9999-9999");
   $("#cepInput").inputmask("99 999-999");
   $("#cnpjInput").inputmask("99.999.999/9999-99");
+
+  $('#numeroInput').on('input', function() {
+    $(this).val(function (index, value) {
+        return value.replace(/\D/g, '');
+    });
+  });
+
+  $("#emailInput").on("input", function () {
+    var email = $(this).val();
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailRegex.test(email)) {
+      $("#emailMessage")
+        .text("Endereço de e-mail válido")
+        .css("color", "green");
+    } else {
+      $("#emailMessage")
+        .text("Endereço de e-mail inválido")
+        .css("color", "red");
+    }
+  });
 });
