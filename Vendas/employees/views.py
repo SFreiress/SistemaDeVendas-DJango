a87fa244b5dc574854a96c64cc0c54e0ab860e76
Sérgio Freire Soares <sergiofss_2005@hotmail.com>
2023-11-14 21:29:25 -0300

    WIP: employees

--- Vendas/employees/views.py
+++ Vendas/employees/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Funcionario
 
 
@@ -6,11 +6,6 @@ from .models import Funcionario
 def home(request):
     funcionarios = Funcionario.objects.all()
     return render(request, "employees.html", {"funcionarios": funcionarios})
-
-def registerEmployee(request):
-    return render(request, "employee.html")
-
-
 
 def salvar(request):
     if request.method == 'POST':
@@ -31,6 +26,28 @@ def salvar(request):
             cidade=request.POST.get("cidade"),
         )
         return redirect('employees:home')
+    return render(request, "registerEmployee.html")
+
+def atualizar(request, id):
+    if request.method == 'POST':
+        funcionario = get_object_or_404(Funcionario, id=id)
+        funcionario.nome=request.POST.get("nome")
+        # funcionario.rg=request.POST.get("rg")
+        funcionario.cpf=request.POST.get("cpf")
+        funcionario.email=request.POST.get("email")
+        # funcionario.senha=request.POST.get("senha")
+        # funcionario.cargo=request.POST.get("cargo")
+        # funcionario.nivel_acesso=request.POST.get("nivel_acesso")
+        funcionario.celular=request.POST.get("celular")
+        funcionario.cep=request.POST.get("cep")
+        funcionario.endereco=request.POST.get("endereco")
+        funcionario.numero=request.POST.get("numero")
+        funcionario.complemento=request.POST.get("complemento")
+        funcionario.bairro=request.POST.get("bairro")
+        funcionario.cidade=request.POST.get("cidade")
+        
+        funcionario.save()
+        return redirect('employees:home')
     
-    funcionarios = Funcionario.objects.all()
-    return render(request, "employees.html", {"funcionarios": funcionarios})
+    funcionarios = Funcionario.objects.get(id=id)
+    return render(request, "updateEmployee.html", {"funcionario": funcionarios})
---
+++ Vendas/static/employees/js/input_patterns.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#telefoneInput").inputmask("(99) 9999-9999");
+    $("#celularInput").inputmask("(99) 9 9999-9999");
+    $("#cepInput").inputmask("99.999-999");
+    $("#cpfInput").inputmask("999.999.999-99");
+  });
