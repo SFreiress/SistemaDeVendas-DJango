c36ba8e523aa5be477f08b82a7b2845431dee5a2
Mayhm <nisekoimylove@gmail.com>
2023-11-16 18:59:44 -0300

    feat: cep finder para todas as telas necessárias

--- Vendas/static/clients/js/input_patterns.js
+++ Vendas/static/clients/js/input_patterns.js
@@ -19,28 +19,4 @@ $(document).ready(function () {
         .css("color", "red");
     }
   });
-
-  $("#cepInput").on("keypress", async function (e) {
-    console.log("CAIU" + e.which);
-    if (e.which === 13) {
-      var cep = $(this)
-        .val()
-        .replace(/[^0-9]/g, "");
-
-      try {
-        const request = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        const data = await request.json();
-
-        $("#enderecoInput").val(data.logradouro);
-        $("#bairroInput").val(data.bairro);
-        $("#cidadeInput").val(data.localidade);
-        $("#ufInput").val(data.uf);
-        $("#estadoInput").val(data.uf);
-
-        console.log(data);
-      } catch (error) {
-        console.error("Erro na requisição:", error);
-      }
-    }
-  });
 });
--- Vendas/static/employees/js/input_patterns.js
+++ Vendas/static/employees/js/input_patterns.js
@@ -3,28 +3,4 @@ $(document).ready(function () {
   $("#celularInput").inputmask("(99) 9 9999-9999");
   $("#cepInput").inputmask("99.999-999");
   $("#cpfInput").inputmask("999.999.999-99");
-
-  $("#cepInput").on("keypress", async function (e) {
-    console.log("CAIU" + e.which);
-    if (e.which === 13) {
-      var cep = $(this)
-        .val()
-        .replace(/[^0-9]/g, "");
-
-      try {
-        const request = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        const data = await request.json();
-
-        $("#enderecoInput").val(data.logradouro);
-        $("#bairroInput").val(data.bairro);
-        $("#cidadeInput").val(data.localidade);
-        $("#ufInput").val(data.uf);
-        $("#estadoInput").val(data.uf);
-
-        console.log(data);
-      } catch (error) {
-        console.error("Erro na requisição:", error);
-      }
-    }
-  });
 });
